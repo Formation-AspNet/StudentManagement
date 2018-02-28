eb44fc814ea4147a8655ecbe7a25fc68687fe9ca
ES-SARRAJ <essarraj.fouad@gmail.com>
2018-02-28 12:04:34 +0000

    Solution 2  : Model binfing

--- StudentManagement/Controllers/ArithmeticOperationController.cs
+++ StudentManagement/Controllers/ArithmeticOperationController.cs
@@ -14,6 +14,25 @@ namespace StudentManagement.Controllers
             return View();
         }
 
+        // Solutoin 1
+        /*
+        public ActionResult Addition()
+        {
+
+            float? x = float.Parse( Request.Params["x"]);
+            float? y = float.Parse(Request.Params["y"]);
+            if (x != null && y != null)
+            {
+                ViewBag.x = x;
+                ViewBag.y = y;
+                ViewBag.result_addition = x + y;
+
+            }
+            return View();
+        }
+        */
+ 
+        // Solution 2 : avec ModelBinding
         public ActionResult Addition(float? x = null, float? y = null)
         {
             if (x != null && y != null)
@@ -26,4 +45,5 @@ namespace StudentManagement.Controllers
             return View();
         }
     }
+    
 }
